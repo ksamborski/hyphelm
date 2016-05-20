5c69d7cb8beb6b9ee0ee8da849ef0dbd13875a50
Karol Samborski <k.samborski@teb-akademia.pl>
2016-05-20 12:13:53 +0200

    Test template

---
+++ test/Test.elm
@@ -0,0 +1,19 @@
+module Main exposing (main)
+
+import String
+
+import ElmTest exposing (..)
+
+
+tests : Test
+tests = 
+    suite "A Test Suite"
+        [ test "Addition" (assertEqual (3 + 7) 10)
+        , test "String.left" (assertEqual "a" (String.left 1 "abcdefg"))
+        , test "This test should fail" (assert False)
+        ]
+
+
+main : Program Never
+main = 
+    runSuiteHtml tests
